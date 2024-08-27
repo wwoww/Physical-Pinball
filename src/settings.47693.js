413e95d3f2b034884a4a5214a1828cc2c90c31a2
wwoww <woshifish1@163.com>
2024-08-27 15:25:16 +0800

    Add files via upload

---
+++ src/settings.47693.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ballInGame","ballInRecycle"],collisionMatrix:[[true,true,true],[true,false],[true,false,true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"7a0d6"}};
